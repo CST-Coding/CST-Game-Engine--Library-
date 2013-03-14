fd8d365909345d0496e0ee1d7686b664fbb6b3ef
cstcodingTeam <cstcodingteam@gmail.com>
2013-03-14 14:04:34 -0400

    testing opengl

---
+++ common/testing/hi.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class hi {
+	
+	public static void main(String[] argv) {
+		FullscreenExample fullscreenExample = new FullscreenExample();
+		fullscreenExample.start();
+	}
+	
+}
